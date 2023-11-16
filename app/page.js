7b3a94379090d6b3a0df052cacce42f7903fff1e
Ishan Shah <ishan0102@utexas.edu>
2023-11-16 01:13:10 -0500

    Update nav styling

--- app/page.js
+++ app/page.js
@@ -1,30 +1,29 @@
 "use client"
 import React, { useState } from 'react';
-import { Links } from '../components/Links';
 import { Papers } from '../components/Papers';
+import { Links } from '../components/Links';
 
 export default function Home() {
   // State to toggle between Links and Papers
-  const [showLinks, setShowLinks] = useState(true);
+  const [showPapers, setShowPapers] = useState(true);
 
   return (
     <main>
       <div className="flex justify-center space-x-4 my-4">
         <button
-          className={`px-4 py-2 text-gray-900 font-medium group hover:text-indigo-600 ${showLinks ? 'bg-gray-200' : ''}`}
-          onClick={() => setShowLinks(true)}
-        >
-          Links
-        </button>
-        <button
-          className={`px-4 py-2 text-gray-900 font-medium group hover:text-indigo-600 ${!showLinks ? 'bg-gray-200' : ''}`}
-          onClick={() => setShowLinks(false)}
+          className={`px-4 py-2 text-gray-900 font-medium group hover:text-indigo-600 ${showPapers ? 'text-indigo-600' : ''}`}
+          onClick={() => setShowPapers(true)}
         >
           Papers
         </button>
+        <button
+          className={`px-4 py-2 text-gray-900 font-medium group hover:text-indigo-600 ${!showPapers ? 'text-indigo-600' : ''}`}
+          onClick={() => setShowPapers(false)}
+        >
+          Links
+        </button>
       </div>
-
-      {showLinks ? <Links /> : <Papers />}
+      {showPapers ? <Papers /> : <Links />}
     </main>
   );
 }
