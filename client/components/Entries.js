a8b8b34becb203190614108defe114327074f6a9
Ishan Shah <ishan0102@utexas.edu>
2023-11-19 14:24:03 -0500

    Add shuffle button

--- client/components/Entries.js
+++ client/components/Entries.js
@@ -44,6 +44,13 @@ export function Entries({ database }) {
     setSearchTerm(event.target.value.toLowerCase());
   };
 
+  const handleShuffleClick = () => {
+    if (filteredEntries.length > 0) {
+      const randomEntry = filteredEntries[Math.floor(Math.random() * filteredEntries.length)];
+      window.location.href = randomEntry.url.replace("pdf", "abs");
+    }
+  };
+
   const filteredEntries = entries.filter(entry => 
     entry.title.toLowerCase().includes(searchTerm)
   );
@@ -55,8 +62,15 @@ export function Entries({ database }) {
           type="text"
           placeholder="Search"
           onChange={handleSearchChange}
-          className="p-2 border border-gray-400 rounded-md mb-4 bg-[#f0eadd] placeholder-gray-500"
+          className="placeholder-gray-600 p-2 border border-gray-400 rounded-md mb-4 bg-[#f0eadd]"
         />
+        <button
+          onClick={handleShuffleClick}
+          className="flex items-center justify-center gap-2 text-gray-600 p-2 border border-gray-400 rounded-md mb-4 ml-2 float-right bg-[#f0eadd] hover:bg-indigo-100"
+        >
+          Shuffle
+          <svg width="18" height="18" viewBox="0 0 92 92"><path d="M68.2 30C64 30 53.5 40.4 45.8 47.9 34.2 59.1 23.2 70 14.4 70H9c-2.2 0-4-1.8-4-4s1.8-4 4-4h5.4c5.6 0 16.8-11.2 25.8-19.9 11-10.6 20.5-20.1 28-20.1h5.2l-5.5-5.3c-1.6-1.6-1.6-3.9 0-5.5s4.1-1.6 5.7 0l12.3 12.3c.8.8 1.2 1.8 1.2 2.8 0 1.1-.4 2.1-1.2 2.8L73.5 41.4c-.8.8-1.8 1.1-2.8 1.1-1 0-2-.4-2.8-1.2-1.6-1.6-1.6-4.2 0-5.7l5.5-5.6h-5.2zm5.3 20.6c-1.6-1.6-4.1-1.6-5.7 0-1.6 1.6-1.6 4.2 0 5.8l5.5 5.6h-5.2c-3.3 0-8.8-4.9-14.2-9.9-1.6-1.5-4.1-1.5-5.7.1-1.5 1.6-1.4 4.3.2 5.8 8.4 7.7 14 12 19.8 12h5.2l-5.5 5.3c-1.6 1.6-1.6 4 0 5.6.8.8 1.8 1.1 2.8 1.1 1 0 2-.4 2.8-1.2l12.3-12.3c.8-.8 1.2-1.8 1.2-2.8 0-1.1-.4-2.1-1.2-2.8L73.5 50.6zM9 30h5.4c4.2 0 10.5 5.4 15.5 9.9.8.7 1.7 1.1 2.7 1.1 1.1 0 2.2-.4 3-1.3 1.5-1.6 1.4-4.3-.3-5.8-7.8-7.1-14.4-12-20.9-12H9c-2.2 0-4 1.8-4 4S6.8 30 9 30z"></path></svg>
+        </button>
         {filteredEntries.map((entry, index) => (
           <Entry
             key={index}
