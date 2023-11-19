b63e9a7578280ef2a2f9a315070a9a9fed979abb
Ishan Shah <ishan0102@utexas.edu>
2023-11-19 01:17:06 -0500

    Implement working Supabase queries on frontend

--- client/components/Links.js
+++ client/components/Links.js
@@ -1,48 +1,49 @@
-const DummyLinks = [
-  {
-    name: "link 1",
-    date: "2023",
-    tagline: "Stream of my favorite papers and links",
-    link: "https://www.rsrch.space/"
-  },
-  {
-    name: "link 2",
-    date: "2023",
-    tagline: "Browse the web with GPT-4V and Vimium",
-    link: "https://github.com/ishan0102/vimGPT"
-  },
-  {
-    name: "link 3",
-    date: "2023",
-    tagline: "Zettelkasten for my brain using Quartz",
-    link: "https://www.ishan.coffee/"
-  },
-];
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
 
-function Link({ name, date, tagline, link }) {
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+function Link({ title, created, link }) {
+  const dateObj = new Date(created);
+  const formattedDate = dateObj.toISOString().split('T')[0];
   return (
-    <a href={link} className="flex justify-between text-secondary py-1 group">
-      <strong className="flex-none font-medium text-gray-900 group-hover:text-indigo-600 dark:text-gray-100 dark:group-hover:text-indigo-500">{name}</strong>
-      <p className="ml-auto hidden sm:inline mr-8">{tagline}</p>
-      <p>{date}</p>
+    <a href={link.replace("pdf", "abs")} className="flex justify-between text-secondary py-1 group text-md">
+      <strong className="flex-none font-medium font-berkeley text-gray-900 group-hover:text-indigo-600 dark:text-gray-100 dark:group-hover:text-indigo-500">{formattedDate}</strong>
+      <p className="mr-auto pl-12">{title}</p>
     </a>
-  )
+  );
 }
 
 export function Links() {
+  const [links, setLinks] = useState([]);
+
+  async function fetchPosts() {
+    const { data } = await supabase
+      .from('links')
+      .select('*')
+      .order('notion_timestamp', { ascending: false });
+
+    setLinks(data);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className="max-h-screen w-full flex-col overflow-y-scroll scrollbar-hide px-8">
-      <div className="mx-auto w-full max-w-2xl mt-4 sm:mt-8 mb-52 md:mb-32">
-        {DummyLinks.map((project, index) => {
-          return <Link
+      <div className="mx-auto w-full max-w-5xl mb-52 md:mb-32">
+        {links.map((link, index) => (
+          <Link
             key={index}
-            name={project.name}
-            date={project.date}
-            tagline={project.tagline}
-            link={project.link}
+            title={link.title}
+            created={link.notion_timestamp}
+            link={link.url}
           />
-        })}
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- client/components/Papers.js
+++ client/components/Papers.js
@@ -1,45 +1,46 @@
-const DummyPapers = [
-  {
-    name: "paper 1",
-    date: "2023",
-    tagline: "Stream of my favorite papers and links",
-    link: "https://www.rsrch.space/"
-  },
-  {
-    name: "paper 2",
-    date: "2023",
-    tagline: "Browse the web with GPT-4V and Vimium",
-    link: "https://github.com/ishan0102/vimGPT"
-  },
-  {
-    name: "paper 3",
-    date: "2023",
-    tagline: "Zettelkasten for my brain using Quartz",
-    link: "https://www.ishan.coffee/"
-  },
-];
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
 
-function Paper({ name, date, tagline, link }) {
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+function Paper({ title, created, link }) {
+  const dateObj = new Date(created);
+  const formattedDate = dateObj.toISOString().split('T')[0];
   return (
-    <a href={link} className="flex justify-between text-secondary py-1 group">
-      <strong className="flex-none font-medium text-gray-900 group-hover:text-indigo-600 dark:text-gray-100 dark:group-hover:text-indigo-500">{name}</strong>
-      <p className="ml-auto hidden sm:inline mr-8">{tagline}</p>
-      <p>{date}</p>
+    <a href={link.replace("pdf", "abs")} className="flex justify-between text-secondary py-1 group text-md">
+      <strong className="flex-none font-medium font-berkeley text-gray-900 group-hover:text-indigo-600 dark:text-gray-100 dark:group-hover:text-indigo-500">{formattedDate}</strong>
+      <p className="mr-auto pl-12">{title}</p>
     </a>
   );
 }
 
 export function Papers() {
+  const [papers, setPapers] = useState([]);
+
+  async function fetchPosts() {
+    const { data } = await supabase
+      .from('papers')
+      .select('*')
+      .order('notion_timestamp', { ascending: false });
+
+    setPapers(data);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className="max-h-screen w-full flex-col overflow-y-scroll scrollbar-hide px-8">
-      <div className="mx-auto w-full max-w-2xl mt-4 sm:mt-8 mb-52 md:mb-32">
+      <div className="mx-auto w-full max-w-5xl mb-52 md:mb-32">
         {papers.map((paper, index) => (
           <Paper
             key={index}
-            name={paper.name}
-            date={paper.date}
-            tagline={paper.tagline}
-            link={paper.link}
+            title={paper.title}
+            created={paper.notion_timestamp}
+            link={paper.url}
           />
         ))}
       </div>
