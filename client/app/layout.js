337dc03ae751220a4c7b015107a8b5c258e146bd
Ishan Shah <ishan0102@utexas.edu>
2023-11-19 18:43:52 -0500

    Add favicon

--- client/app/layout.js
+++ client/app/layout.js
@@ -9,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <GoogleAnalytics />
         {children}
